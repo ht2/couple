21de5a4800e226db53716af49018a1221ffba201
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-02 12:46:51 +0100

    Corrects tests.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,7 @@
+<?php
+
+class ExampleTest extends PHPUnit_Framework_TestCase {
+  public function testNumbers() {
+    $this->assertEquals(1, 1);
+  }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once(__DIR__ . '/../src/couple.php');
+include_once(__DIR__ . '/../src/TypedCouple.php');
+include_once(__DIR__ . '/../src/Match.php');
+include_once(__DIR__ . '/../src/Merge.php');
+include_once(__DIR__ . '/../src/Validate.php');
