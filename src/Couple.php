4d4cc6f36f867d7785ee1ac06c3576a91fad65b3
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-09 12:13:28 +0100

    Adds doc to `Couple`.

--- src/Couple.php
+++ src/Couple.php
@@ -1,6 +1,13 @@
 <?php namespace ht2\couple;
 
 class Couple {
+  /**
+   * Calls the modifier with a needle and haystack.
+   * @param mixed $needle the needle to pass to the modifier.
+   * @param mixed $haystack the haystack to pass to the modifier.
+   * @param callable $modifier a function to be called with the needle and haystack.
+   * @return mixed returns the result of calling the modifier with the needle and haystack.
+   */
   public function run($needle, $haystack, $modifier) {
     return $modifier($needle, $haystack);
   }
